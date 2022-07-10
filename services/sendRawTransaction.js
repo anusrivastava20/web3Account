74e941232c167dd82d6faeda6070592d2910bd33
anu <priya.anusrivastava@gmail.com>
2022-07-10 16:22:03 +0530

    removed the unwanted data

--- services/sendRawTransaction.js
+++ services/sendRawTransaction.js
@@ -1,6 +1,6 @@
 const Tx = require('@ethereumjs/tx').Transaction
 var Web3 = require('web3');
-var web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/4c881e151e0e4242b34c276f326429d3'));
+var web3 = new Web3(new Web3.providers.HttpProvider('https://www..io/'));
 const Common = require('@ethereumjs/common')
 const common = new Common.default({ chain: 'rinkeby' })
 var sendSignedTransaction = (req, res) => {
--- services/wallet.js
+++ services/wallet.js
@@ -1,7 +1,6 @@
 var Web3 = require('web3');
 //var web3 = new Web3 (Web3.givenProvider || new Web3.providers.WebsocketProvider("ws://localhost:8545"));
-var web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/4c881e151e0e4242b34c276f326429d3'));
-
+var web3 = new Web3(new Web3.providers.HttpProvider('https://www..io/'));
 var createWallet = async(number) => {
     var newWallet = await web3.eth.accounts.wallet.create(parseInt(number));
     console.log('%s',newWallet);
