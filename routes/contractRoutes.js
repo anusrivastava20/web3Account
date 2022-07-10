e3481baf3bb6bd1f2695fbbd13b775c945a9c66f
anu <priya.anusrivastava@gmail.com>
2022-07-10 16:00:06 +0530

    added the fix

--- routes/contractRoutes.js
+++ routes/contractRoutes.js
@@ -7,5 +7,7 @@ const contract_controller = require('../controller/contractController');
 // create a new accounts
 router.post('/getDetails', contract_controller.get_contract_details);
 router.post('/signedTran', contract_controller.send_sign_tran);
+router.post('/deployContract', contract_controller.deploy_contract);
+
 
 module.exports = router;
